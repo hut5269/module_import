126af6c8fe831c28b1d8da0e3b06f0ce4f69b277
Jason Hutchinson <59323131+hut5269@users.noreply.github.com>
2023-07-28 11:19:42 -0400

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -1,2 +1,17 @@
-def print_message(message)
+from google.colab import files
+from IPython.display import clear_output
+
+# def odd_name(message):
+#     clear_output()
+#     print(message)
+#     # %cd /content/Input
+#     user_file = files.upload()
+#     # %cd /content/
+#     clear_output()
+#     return '/content/Input/{0}'.format(next(iter(user_file)))
+
+def print_message_f(message):
+  print(message)
+
+def print_message_g(message):
   print(message)
