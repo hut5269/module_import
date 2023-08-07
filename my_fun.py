143734e2cbe0a390072db293970bbfccc7c0c4e3
Jason Hutchinson <59323131+hut5269@users.noreply.github.com>
2023-08-06 20:09:13 -0400

    Update my_fun.py

--- my_fun.py
+++ my_fun.py
@@ -46,10 +46,33 @@ def simulations():
     sim_df.to_csv('/content/Output/Simulation_Files/Collection-Simulation.csv', 
                     encoding='utf-8', index=False)
 
-def boundary_temp():
+def boundary():
+    # df = pd.read_csv ('/content/Input/Node - Time Series.csv')
     user_file = file_upload("Upload fill model time series data")
     df = pd.read_csv(user_file)
-    simulations = df.Sim.unique().tolist().sort()
-    nodes = df['Node Name'].unique().tolist().sort()
-    BoundarySetStage = pd.DataFrame(simulations,columns =['Names'])
-    print(BoundarySetStage)
+    boundary_stage_point = df.reindex(df.columns.tolist() + ['Year', 'Month', 'Day'], axis=1)
+    boundary_stage_point = boundary_stage_point.rename(columns={'Sim': 'Set', 
+                                                                'Node Name': 'Parent', 
+                                                                'Relative Time [hrs]': 'Hour', 
+                                                                'Stage [ft]': 'Stage'})
+    boundary_stage_point = boundary_stage_point[['Set', 'Parent', 'Year', 'Month', 'Day', 'Hour', 'Stage']]
+
+    nodes = df['Node Name'].unique()
+    simulations = df['Sim'].unique()
+
+    boundary_stage_set = pd.DataFrame(simulations, columns =['Name']) 
+    boundary_stage_set['Comment'] = np.nan
+   
+    combined_list = []
+    for sim in simulations:
+        for node in nodes:
+            row = [sim, node, '']
+            combined_list.append(row)
+    boundary_stage = pd.DataFrame(combined_list, columns =['Parent', 'Name', 'Comment']) 
+
+    boundary_stage_point.to_csv('/content/Output/Boundary_Files/Collection-BoundaryStage_Point.csv',
+                    encoding='utf-8', index=False)
+    boundary_stage_set.to_csv('/content/Output/Boundary_Files/Collection-BoundaryStageSet.csv',
+                    encoding='utf-8', index=False)
+    boundary_stage.to_csv('/content/Output/Boundary_Files/Collection-BoundaryStage.csv',
+                    encoding='utf-8', index=False)
